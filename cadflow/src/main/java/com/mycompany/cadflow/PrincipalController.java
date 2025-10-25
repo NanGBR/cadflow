498a5fefcdf57470e9a10597c243f9e6523ef9b4
Alysson <Alysson@AcerNitroV15>
2025-10-24 23:20:08 -0300

    Alteração de Telas

--- cadflow/src/main/java/com/mycompany/cadflow/PrincipalController.java
+++ cadflow/src/main/java/com/mycompany/cadflow/PrincipalController.java
@@ -6,7 +6,12 @@ package com.mycompany.cadflow;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.image.ImageView;
     //teste
 /**
  * FXML Controller class
@@ -15,9 +20,33 @@ import javafx.fxml.Initializable;
  */
 public class PrincipalController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private Button btnCadastroPessoa;
+
+    @FXML
+    private Button btnEditar;
+
+    @FXML
+    private Button btnExcluir;
+
+    @FXML
+    private Button btnFicha;
+
+    @FXML
+    private Button btnImprimir;
+
+    @FXML
+    private ImageView imgCadFlow;
+
+    @FXML
+    private TableColumn<?, ?> tableViewAcolhidosCodigo;
+
+    @FXML
+    private TableColumn<?, ?> tableViewAcolhidosNome;
+
+    @FXML
+    private Label txtTelaPrincipalCadFlow;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
